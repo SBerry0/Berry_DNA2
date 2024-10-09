bf24ae7ec083041b8e8c7eb3da0927571ec3bb15
Sohum Berry <sohumberry@gmail.com>
2024-10-09 09:59:21 -0700

    First Log

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,8 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date  |     Time     |                                                                                                                                                                                                                                                                              Update |
+|:------|:------------:|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Oct 9 | 9:25-10:00am | I made my first try on the DNA part two using indexOf. I wasn't really expecting it to do anything in terms of fully solving it, I just wanted to write stuff. I don't know if I'm going to continue debugging it or trying to do a new solutioin that goes character by character. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/DNA.java
+++ src/DNA.java
@@ -16,7 +16,38 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
+        return takeOne(sequence, STR);
 
-        return 0;
+
+
+//        return 0;
+    }
+
+    public static int takeOne(String sequence, String STR) {
+        int index = 0;
+        String checkString = STR;
+        int streak = 0;
+        int maxStreak = streak;
+        while (true) {
+            index = sequence.indexOf(checkString);
+            if (index == -1)
+                break;
+            sequence = sequence.substring(index+checkString.length());
+            streak++;
+            if (index+checkString.length()+1 >= sequence.length()) {
+                maxStreak = Math.max(streak, maxStreak);
+                break;
+            }
+//            System.out.println(checkString.charAt(0));
+//            System.out.println(sequence.charAt(index+checkString.length()+1));
+            if (sequence.charAt(0) != checkString.charAt(0)) {
+                maxStreak = Math.max(streak, maxStreak);
+                for (int i = 1; i < streak; i++) {
+                    checkString += checkString;
+                }
+                streak = 0;
+            }
+        }
+        return maxStreak;
     }
 }
